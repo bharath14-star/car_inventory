e368581170e8f1055b14ba4078bea5b8b92ec2ca
Bharath Kumar <bharath14star@gmail.com>
2025-11-11 11:21:11 +0530

    Remove localhost MongoDB connection and use environment variable for cluster

--- backend/controllers/config/Database.js
+++ backend/controllers/config/Database.js
@@ -1,17 +1,11 @@
 const mongoose = require('mongoose');
 
-const MONGO_URI = "mongodb+srv://bharath:bharath@cluster0.mcnlrgp.mongodb.net/?appName=Cluster0"
-const MONGO_LOCAL_URI = "mongodb://localhost:27017/car_portal";
+const MONGO_URI = process.env.MONGO_URI || "mongodb+srv://bharath:bharath@cluster0.mcnlrgp.mongodb.net/?appName=Cluster0"
 
-// Establish two connections
+// Establish connection to MongoDB cluster
 const atlasConnection = mongoose.createConnection(MONGO_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
 
-const localConnection = mongoose.createConnection(MONGO_LOCAL_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
-
-module.exports = { atlasConnection, localConnection, MONGO_URI };
+module.exports = { atlasConnection, MONGO_URI };
